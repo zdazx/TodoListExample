85dc8d63fa2349e8fa2431d46556660f7da16c3c
zdazx <zhangduo0227@163.com>
2020-07-29 21:43:07 +0800

    add username and password error hint and button's styles

--- app/src/main/java/com/thoughtworks/todolistexample/MainActivity.java
+++ app/src/main/java/com/thoughtworks/todolistexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.thoughtworks.todolistexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText usernameView = findViewById(R.id.username);
+        EditText passwordView = findViewById(R.id.password);
+        Button loginBtn = findViewById(R.id.login);
+
+        usernameView.addTextChangedListener(
+                new LoginWatcher(usernameView, passwordView, loginBtn, getApplicationContext()));
+        passwordView.addTextChangedListener(
+                new LoginWatcher(usernameView, passwordView, loginBtn, getApplicationContext()));
     }
 }
