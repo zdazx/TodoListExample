d665db6f76f56282669dffec2464404b6bcc32b6
zdazx <zhangduo0227@163.com>
2020-07-30 13:05:12 +0800

    add user class

---
+++ app/src/main/java/com/thoughtworks/todolistexample/repository/user/entity/User.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.todolistexample.repository.user.entity;
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
