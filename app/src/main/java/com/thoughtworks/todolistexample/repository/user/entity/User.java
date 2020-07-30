558c8c1b060488f85202851e5d3dac181b7deef6
zdazx <zhangduo0227@163.com>
2020-07-30 14:00:26 +0800

    add constructor and getter in user class

--- app/src/main/java/com/thoughtworks/todolistexample/repository/user/entity/User.java
+++ app/src/main/java/com/thoughtworks/todolistexample/repository/user/entity/User.java
@@ -2,6 +2,7 @@ package com.thoughtworks.todolistexample.repository.user.entity;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity
@@ -14,6 +15,22 @@ public class User {
 
     @ColumnInfo
     private String password;
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    @Ignore
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
