6499785aba1a9cc39a9b99949073e9f1149742f3
zdazx <zhangduo0227@163.com>
2020-07-30 15:21:22 +0800

    update login implement: first find from db, if find, just go to next activity. If not find from db, get remote user based on url, and insert user into db.

--- app/src/main/java/com/thoughtworks/todolistexample/ui/login/LoginViewModel.java
+++ app/src/main/java/com/thoughtworks/todolistexample/ui/login/LoginViewModel.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.todolistexample.ui.login;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -10,6 +12,8 @@ import com.thoughtworks.todolistexample.repository.user.entity.User;
 import java.io.IOException;
 import java.util.Objects;
 
+import io.reactivex.CompletableObserver;
+import io.reactivex.MaybeObserver;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -22,12 +26,14 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+import static com.thoughtworks.todolistexample.constant.Constants.LOG_TAG;
 import static com.thoughtworks.todolistexample.repository.utils.Encryptor.md5;
 
 public class LoginViewModel extends ViewModel {
     private MutableLiveData<Boolean> loginResult;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private UserRepository userRepository;
+    private User localUser;
 
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -41,24 +47,43 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void login(String username, String password) {
-        Observable.create(new ObservableOnSubscribe<Boolean>() {
-            @Override
-            public void subscribe(ObservableEmitter<Boolean> emitter) throws Exception {
-                final String url = "https://twc-android-bootcamp.github.io/fake-data/data/user.json";
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder().url(url).build();
-                Response response = client.newCall(request).execute();
-
-                if (response.isSuccessful()) {
-                    if (!emitter.isDisposed()) {
-                        emitter.onNext(isLoginSuccess(response, username, password));
+        userRepository.findByName(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe(new MaybeObserver<User>() {
+                    private User dbUser;
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        compositeDisposable.add(d);
                     }
-                    emitter.onComplete();
-                } else if (!response.isSuccessful() && !emitter.isDisposed()) {
-                    emitter.onError(new Exception("error"));
-                }
-            }
-        }).subscribeOn(Schedulers.io())
+
+                    @Override
+                    public void onSuccess(User user) {
+                        dbUser = user;
+                        localUser = user;
+                        loginResult.postValue(true);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        if (Objects.nonNull(localUser) && Objects.nonNull(dbUser)) {
+                            Log.d(LOG_TAG.name(), "success find user from db");
+                            return;
+                        }
+                        Log.d(LOG_TAG.name(), "failed find user from db");
+                        Log.d(LOG_TAG.name(), "start get remote user");
+                        getRemoteUser(username, password);
+                    }
+                });
+    }
+
+    private void getRemoteUser(String username, String password) {
+        Observable.create((ObservableOnSubscribe<Boolean>) emitter -> LoginViewModel.this.getRemoteUser(emitter, username, password)).subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.io())
                 .subscribe(new Observer<Boolean>() {
                     @Override
@@ -78,6 +103,50 @@ public class LoginViewModel extends ViewModel {
 
                     @Override
                     public void onComplete() {
+                        if (Objects.isNull(localUser)) {
+                            Log.d(LOG_TAG.name(), "get remote user failed");
+                            return;
+                        }
+                        Log.d(LOG_TAG.name(), "start insert user to db");
+//                        saveRemoteUserToLocal();
+                    }
+                });
+
+    }
+
+    private void getRemoteUser(ObservableEmitter<Boolean> emitter, String username, String password) throws IOException {
+        final String url = "https://twc-android-bootcamp.github.io/fake-data/data/user.json";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+
+        if (response.isSuccessful()) {
+            if (!emitter.isDisposed()) {
+                emitter.onNext(isLoginSuccess(response, username, password));
+            }
+            emitter.onComplete();
+        } else if (!response.isSuccessful() && !emitter.isDisposed()) {
+            emitter.onError(new Exception("error"));
+        }
+    }
+
+    private void saveRemoteUserToLocal() {
+        userRepository.save(localUser)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        compositeDisposable.add(d);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(LOG_TAG.name(), "end insert user to db");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
 
                     }
                 });
@@ -88,11 +157,11 @@ public class LoginViewModel extends ViewModel {
         if (body == null) {
             return false;
         }
-        String dbUserJson = body.string();
-        User dbUser = new Gson().fromJson(dbUserJson, User.class);
-        return !Objects.isNull(dbUser)
-                && dbUser.getName().equals(username)
-                && dbUser.getPassword().equals(md5(password));
+        User remoteUser = new Gson().fromJson(body.string(), User.class);
+        this.localUser = remoteUser;
+        return !Objects.isNull(remoteUser)
+                && remoteUser.getName().equals(username)
+                && remoteUser.getPassword().equals(md5(password));
     }
 
     @Override
