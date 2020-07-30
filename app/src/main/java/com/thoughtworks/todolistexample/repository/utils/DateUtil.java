d0334b9159586c97f4b6bc268e320988599ad7b9
zdazx <zhangduo0227@163.com>
2020-07-31 04:33:28 +0800

    add timestamp to date

--- app/src/main/java/com/thoughtworks/todolistexample/repository/utils/DateUtil.java
+++ app/src/main/java/com/thoughtworks/todolistexample/repository/utils/DateUtil.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Objects;
 
 public class DateUtil {
     public static final String INVALID_TIME = "";
@@ -15,5 +16,12 @@ public class DateUtil {
             return INVALID_TIME;
         }
         return String.valueOf(date.getTime());
+    }
+
+    public static String toDate(String timestamp) {
+        if (Objects.isNull(timestamp) || timestamp.equals("")) {
+            return "";
+        }
+        return new SimpleDateFormat("MM月dd日", Locale.CHINA).format(Long.parseLong(timestamp));
     }
 }
