dd519ea38f55640b0e1533c0443fab06ff17d340
zdazx <zhangduo0227@163.com>
2020-07-31 22:56:42 +0800

    add liveData result of result, update task method

--- app/src/main/java/com/thoughtworks/todolistexample/ui/create/CreateViewModel.java
+++ app/src/main/java/com/thoughtworks/todolistexample/ui/create/CreateViewModel.java
@@ -15,6 +15,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public class CreateViewModel extends ViewModel {
     private MutableLiveData<Boolean> createResult;
+    private MutableLiveData<Boolean> updateResult;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private TaskRepository taskRepository;
 
@@ -23,6 +24,13 @@ public class CreateViewModel extends ViewModel {
             createResult = new MutableLiveData<>();
         }
         return createResult;
+    }
+
+    public LiveData<Boolean> getUpdateResult() {
+        if (Objects.isNull(updateResult)) {
+            updateResult = new MutableLiveData<>();
+        }
+        return updateResult;
     }
 
     public void setTaskRepository(TaskRepository taskRepository) {
@@ -51,6 +59,33 @@ public class CreateViewModel extends ViewModel {
                     @Override
                     public void onError(Throwable e) {
                         createResult.postValue(false);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    public void update(Task task) {
+        taskRepository.update(task)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe(new MaybeObserver<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        compositeDisposable.add(d);
+                    }
+
+                    @Override
+                    public void onSuccess(Integer integer) {
+                        updateResult.postValue(true);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        updateResult.postValue(false);
                     }
 
                     @Override
