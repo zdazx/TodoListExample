fb39df5d4861268cb85307f6f2dcacd2deaead02
zdazx <zhangduo0227@163.com>
2020-07-31 18:00:51 +0800

    move HomeViewHolder to HomeAdapter class

--- app/src/main/java/com/thoughtworks/todolistexample/ui/home/HomeAdapter.java
+++ app/src/main/java/com/thoughtworks/todolistexample/ui/home/HomeAdapter.java
@@ -4,6 +4,8 @@ import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,6 +29,19 @@ public class HomeAdapter extends RecyclerView.Adapter {
         this.notifyDataSetChanged();
     }
 
+    private static class HomeViewHolder extends RecyclerView.ViewHolder {
+        public CheckBox selectDoneCheckBox;
+        public TextView titleView;
+        public TextView deadlineView;
+
+        public HomeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            selectDoneCheckBox = itemView.findViewById(R.id.select_done_checkbox);
+            titleView = itemView.findViewById(R.id.task_title);
+            deadlineView = itemView.findViewById(R.id.task_deadline);
+        }
+    }
+
     @NonNull
     @Override
     public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,11 +54,11 @@ public class HomeAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Task task = tasks.get(position);
-        ((HomeViewHolder)holder).getSelectDoneCheckBox().setChecked(task.isDone());
-        ((HomeViewHolder)holder).getTitleView().setText(task.getTitle());
-        ((HomeViewHolder)holder).getDeadlineView().setText(toDate(task.getDeadline()));
+        ((HomeViewHolder)holder).selectDoneCheckBox.setChecked(task.isDone());
+        ((HomeViewHolder)holder).titleView.setText(task.getTitle());
+        ((HomeViewHolder)holder).deadlineView.setText(toDate(task.getDeadline()));
         if (task.isDone()) {
-            ((HomeViewHolder)holder).getTitleView().getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            ((HomeViewHolder)holder).titleView.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
         }
     }
 
