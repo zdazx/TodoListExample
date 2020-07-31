05b936ca811d0aac9651bb7d50e61f479cd1f4d6
zdazx <zhangduo0227@163.com>
2020-07-31 15:56:33 +0800

    move login watcher to login activity

--- app/src/main/java/com/thoughtworks/todolistexample/ui/login/LoginActivity.java
+++ app/src/main/java/com/thoughtworks/todolistexample/ui/login/LoginActivity.java
@@ -2,6 +2,8 @@ package com.thoughtworks.todolistexample.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,10 +17,14 @@ import com.thoughtworks.todolistexample.MainApplication;
 import com.thoughtworks.todolistexample.R;
 import com.thoughtworks.todolistexample.ui.home.HomeActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
     private EditText usernameView;
     private EditText passwordView;
     private LoginViewModel loginViewModel;
+    private Button loginButton;
     private final String LOGIN_ACTIVITY = this.getClass().getSimpleName();
 
     @Override
@@ -28,13 +34,11 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameView = findViewById(R.id.username);
         passwordView = findViewById(R.id.password);
-        Button loginBtn = findViewById(R.id.login);
+        loginButton = findViewById(R.id.login);
 
-        usernameView.addTextChangedListener(
-                new LoginWatcher(usernameView, passwordView, loginBtn, getApplicationContext()));
-        passwordView.addTextChangedListener(
-                new LoginWatcher(usernameView, passwordView, loginBtn, getApplicationContext()));
-        loginBtn.setOnClickListener(view -> login());
+        usernameView.addTextChangedListener(new LoginWatcher());
+        passwordView.addTextChangedListener(new LoginWatcher());
+        loginButton.setOnClickListener(view -> login());
 
         loginViewModel = obtainViewModel();
         final Observer<Boolean> observer = aBoolean -> {
@@ -64,5 +68,49 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login() {
         loginViewModel.login(usernameView.getText().toString(), passwordView.getText().toString());
+    }
+
+    private class LoginWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            setLoginBtnStyle(false, R.color.colorDeepGray, R.drawable.login_button_not_enable);
+
+            if (!isUsernameValid()) {
+                usernameView.setError("用户名必须是3 ~ 12为字母或数字");
+            } else if (!isPasswordValid()) {
+                passwordView.setError("密码长度必须是6 ~ 18位字符");
+            } else {
+                setLoginBtnStyle(true, R.color.colorWhite, R.drawable.login_button_enable);
+            }
+        }
+
+        private boolean isPasswordValid() {
+            String password = passwordView.getText().toString();
+            return password.length() > 5 && password.length() < 19;
+        }
+
+        private boolean isUsernameValid() {
+            String username = usernameView.getText().toString().trim();
+            Pattern pattern = Pattern.compile("[0-9a-zA-Z]{3,12}");
+            Matcher matcher = pattern.matcher(username);
+            return matcher.matches();
+        }
+
+        private void setLoginBtnStyle(boolean isEnable, int textColor, int background) {
+            loginButton.setEnabled(isEnable);
+            loginButton.setTextColor(getColor(textColor));
+            loginButton.setBackground(getDrawable(background));
+        }
     }
 }
