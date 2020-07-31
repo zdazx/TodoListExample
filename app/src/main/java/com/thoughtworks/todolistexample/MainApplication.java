1188f36dfbc9405402f186a6120cbeceb76d02c8
zdazx <zhangduo0227@163.com>
2020-07-31 16:29:48 +0800

    add appdatabase close when application is terminal

--- app/src/main/java/com/thoughtworks/todolistexample/MainApplication.java
+++ app/src/main/java/com/thoughtworks/todolistexample/MainApplication.java
@@ -12,9 +12,12 @@ import com.thoughtworks.todolistexample.repository.user.UserRepositoryImpl;
 import com.thoughtworks.todolistexample.ui.create.TaskRepository;
 import com.thoughtworks.todolistexample.ui.login.UserRepository;
 
+import java.util.Objects;
+
 public class MainApplication extends Application {
     private UserRepository userRepository;
     private TaskRepository taskRepository;
+    private AppDatabase database;
 
     @Override
     public void onCreate() {
@@ -23,14 +26,25 @@ public class MainApplication extends Application {
         taskRepository = new TaskRepositoryImpl(taskDataSource());
     }
 
+    @Override
+    public void onTerminate() {
+        getDatabase().close();
+        super.onTerminate();
+    }
+
+    private AppDatabase getDatabase() {
+        if (Objects.isNull(database)) {
+            database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, this.getClass().getSimpleName()).build();
+        }
+        return database;
+    }
+
     private TaskDataSource taskDataSource() {
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, this.getClass().getSimpleName()).build();
-        return db.dbTaskDataSource();
+        return getDatabase().dbTaskDataSource();
     }
 
     private UserDataSource userDataSource() {
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, this.getClass().getSimpleName()).build();
-        return db.dbUserDataSource();
+        return getDatabase().dbUserDataSource();
     }
 
     public UserRepository getUserRepository() {
