210f98c2863f55c4ab3725a234c7884fcfef16b7
zdazx <zhangduo0227@163.com>
2020-07-31 21:47:48 +0800

    add flag constant to recognize the operation, create and update

--- app/src/main/java/com/thoughtworks/todolistexample/constant/Constants.java
+++ app/src/main/java/com/thoughtworks/todolistexample/constant/Constants.java
@@ -4,7 +4,8 @@ public enum Constants {
     CREATE_FAILED("创建失败"),
     CREATE_SUCCESS("创建成功"),
     MUST_EDIT_TITLE("请将标题填写完整"),
-    MUST_EDIT_DATE("请选择日期");
+    MUST_EDIT_DATE("请选择日期"),
+    IS_HAVE_TASK("isHaveTask");
 
     private String name;
 
