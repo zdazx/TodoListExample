b2b766af879df8ca7debda28ed1f6313b5544852
zdazx <zhangduo0227@163.com>
2020-08-02 15:56:09 +0800

    add buiness constant containing remote user url

---
+++ app/src/main/java/com/thoughtworks/todolistexample/constant/BusinessConstant.java
@@ -0,0 +1,5 @@
+package com.thoughtworks.todolistexample.constant;
+
+public class BusinessConstant {
+    public static final String REMOTE_USER_URL = "https://twc-android-bootcamp.github.io/fake-data/data/user.json";
+}
