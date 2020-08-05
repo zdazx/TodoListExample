acff9be2ef58efd55d631cd56b7c279c21fa6ecc
zdazx <zhangduo0227@163.com>
2020-08-06 00:01:55 +0800

    add constant

--- app/src/main/java/com/thoughtworks/todolistexample/constant/Constants.java
+++ app/src/main/java/com/thoughtworks/todolistexample/constant/Constants.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.todolistexample.constant;
 
 public enum Constants {
-    IS_HAVE_TASK("isHaveTask");
+    IS_HAVE_TASK("isHaveTask"),
+    TASK_KEY("TASK_KEY"),
+    TASK_NOTIFICATION("TASK_NOTIFICATION");
 
     private String name;
 
