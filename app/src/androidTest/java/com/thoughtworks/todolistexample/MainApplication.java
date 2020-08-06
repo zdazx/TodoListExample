f0fdf8334b5a9237e74d89bb6e8884e6c6e5ddbf
zdazx <zhangduo0227@163.com>
2020-08-06 20:52:29 +0800

    add mock task repo in main application of android test

--- app/src/androidTest/java/com/thoughtworks/todolistexample/MainApplication.java
+++ app/src/androidTest/java/com/thoughtworks/todolistexample/MainApplication.java
@@ -2,7 +2,9 @@ package com.thoughtworks.todolistexample;
 
 import android.app.Application;
 
+import com.thoughtworks.todolistexample.repository.task.TaskRepositoryImpl;
 import com.thoughtworks.todolistexample.repository.user.UserRepositoryImpl;
+import com.thoughtworks.todolistexample.ui.create.TaskRepository;
 import com.thoughtworks.todolistexample.ui.login.UserRepository;
 
 import java.util.Objects;
@@ -11,6 +13,7 @@ import static org.mockito.Mockito.mock;
 
 public class MainApplication extends Application {
     private UserRepository userRepository;
+    private TaskRepository taskRepository;
 
     @Override
     public void onCreate() {
@@ -22,5 +25,12 @@ public class MainApplication extends Application {
             userRepository = mock(UserRepositoryImpl.class);
         }
         return userRepository;
+    }
+
+    public TaskRepository getTaskRepository() {
+        if (Objects.isNull(taskRepository)) {
+            taskRepository = mock(TaskRepositoryImpl.class);
+        }
+        return taskRepository;
     }
 }
